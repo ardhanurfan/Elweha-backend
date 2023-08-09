ea08a2007e50221fcf471c6f0cf3c246ee434e6b
Ardhan Nur Urfan <88726616+ardhanurfan@users.noreply.github.com>
2023-08-09 10:43:48 +0700

    feat: akta

--- app/Http/Controllers/API/PajakRekanAktaController.php
+++ app/Http/Controllers/API/PajakRekanAktaController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\API;
 
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
-use App\Models\PajakRekan;
 use App\Models\PajakRekanAkta;
 use Exception;
 use Illuminate\Http\Request;
@@ -23,18 +22,6 @@ class PajakRekanAktaController extends Controller
                 'no_awal' => 'required|integer|min:1',
                 'no_akhir' => 'required|integer|min:' . $request->no_awal,
             ]);
-
-            $pajak_rekan = PajakRekan::where('id', $request->pajak_rekan_id);
-            if (!$pajak_rekan) {
-                return ResponseFormatter::error(
-                    [
-                        'message' => 'Something when wrong',
-                        'error' => 'Rekan not found',
-                    ],
-                    'Create Akta Failed',
-                    404,
-                );
-            }
 
             // Cek Ketersediaan Nomor Akta
             $sold = [];
@@ -60,16 +47,6 @@ class PajakRekanAktaController extends Controller
                 $i++;
             }
 
-            // Perhitungan Pajak Rekan
-            $jumlah_akta = $pajak_rekan->jumlah_akta + ($request->no_akhir - $request->no_awal + 1);
-            $jasa_bruto = $jumlah_akta * $pajak_rekan->biaya_jasa;
-            $dpp = 0.5 * $jasa_bruto;
-            $dpp = 0.5 * $jasa_bruto;
-
-            $pajak_rekan->update([
-                'jumlah_akta' => $jumlah_akta,
-            ]);
-
             $pajak_rekan_akta = PajakRekanAkta::create([
                 'user_id' => Auth::id(),
                 'pajak_rekan_id' => $request->pajak_rekan_id,
@@ -78,8 +55,6 @@ class PajakRekanAktaController extends Controller
                 'no_akhir' => $request->no_akhir,
                 'jumlah_akta' => $request->no_akhir - $request->no_awal + 1,
             ]);
-
-
 
             return ResponseFormatter::success(
                 $pajak_rekan_akta,
@@ -123,7 +98,140 @@ class PajakRekanAktaController extends Controller
             [
                 'table' => $pajak_rekan_akta->orderBy('tanggal', 'DESC')->paginate($limit)
             ],
-            'Get Pendapatan Successfully'
+            'Get Akta Successfully'
         );
+    }
+
+    // UPDATE
+    public function update(Request $request)
+    {
+        try {
+            $request->validate([
+                'id' => 'required',
+                'pajak_rekan_id' => 'required|integer',
+                'tanggal' => 'required|date',
+                'no_awal' => 'required|integer|min:1',
+                'no_akhir' => 'required|integer|min:' . $request->no_awal,
+            ]);
+
+            $pajak_rekan_akta = PajakRekanAkta::find($request->id);
+
+            if (!$pajak_rekan_akta) {
+                return ResponseFormatter::error(
+                    [
+                        'message' => 'Something when wrong',
+                        'error' => 'Data not found',
+                    ],
+                    'Edit Akta Failed',
+                    404,
+                );
+            }
+
+            // Cek Ketersediaan Nomor Akta, kecuali diirinya sendiri
+            $sold = [];
+            $get_no = PajakRekanAkta::select('no_awal', 'no_akhir')->where('id', '!=', $request->id)->get();
+            foreach ($get_no as $row) {
+                for ($i = $row->no_awal; $i <= $row->no_akhir; $i++) {
+                    array_push($sold, $i);
+                }
+            }
+
+            $i = $request->no_awal;
+            while ($i <= $request->no_akhir) {
+                if (in_array($i, $sold)) {
+                    return ResponseFormatter::error(
+                        [
+                            'message' => 'Something when wrong',
+                            'error' => 'Akta number not available',
+                        ],
+                        'Create Akta Failed',
+                        404,
+                    );
+                }
+                $i++;
+            }
+
+            $pajak_rekan_akta->update([
+                'user_id' => Auth::id(),
+                'pajak_rekan_id' => $request->pajak_rekan_id,
+                'tanggal' => $request->tanggal,
+                'no_awal' => $request->no_awal,
+                'no_akhir' => $request->no_akhir,
+                'jumlah_akta' => $request->no_akhir - $request->no_awal + 1,
+            ]);
+
+            return ResponseFormatter::success(
+                $pajak_rekan_akta,
+                'Edit Akta Successfully'
+            );
+        } catch (ValidationException $error) {
+            return ResponseFormatter::error(
+                [
+                    'message' => 'Something when wrong',
+                    'error' => array_values($error->errors())[0][0],
+                ],
+                'Edit Akta Failed',
+                400,
+            );
+        } catch (Exception $error) {
+            return ResponseFormatter::error(
+                [
+                    'message' => 'Something when wrong',
+                    'error' => $error,
+                ],
+                'Edit Akta Failed',
+                500,
+            );
+        }
+    }
+
+    // DELETE
+    public function delete(Request $request)
+    {
+        try {
+            $request->validate([
+                'selectedId' => 'required|array',
+            ]);
+
+            foreach ($request->selectedId as $id) {
+                $pajak_rekan_akta = PajakRekanAkta::find($id);
+
+                if (!$pajak_rekan_akta) {
+                    return ResponseFormatter::error(
+                        [
+                            'message' => 'Something when wrong',
+                            'error' => 'Some Data Not Found',
+                        ],
+                        'Delete Akta Failed',
+                        404,
+                    );
+                }
+
+                $pajak_rekan_akta->forceDelete();
+            }
+
+            return ResponseFormatter::success(
+                null,
+                'Delete Akta Successfully'
+            );
+        } catch (ValidationException $error) {
+            return ResponseFormatter::error(
+                [
+                    'message' => 'Something when wrong',
+                    'error' => array_values($error->errors())[0][0],
+                ],
+                'Delete Akta Failed',
+                400,
+            );
+        } catch (Exception $error) {
+            return ResponseFormatter::error(
+                [
+                    'message' => 'Something when wrong',
+                    'error' => $error,
+                ],
+                'Delete Akta Failed',
+                500,
+            );
+        }
     }
 }
